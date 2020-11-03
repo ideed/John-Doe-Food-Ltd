9aa8844ed99fe059cfa70d0f858f2e7de78c414b
ideed <emanuelivan45@gmail.com>
2020-11-03 11:50:28 +0000

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="John_Doe_Food_Ltd.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(John_Doe_Food_Ltd.Startup))]
+namespace John_Doe_Food_Ltd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
